1fb39b79121c3e93e4294b9631b61f26120550b6
Giulia <gxahid234@gmail.com>
2019-04-17 21:17:07 -0300

    fiz os exercicios

---
+++ curriculo/javascript/javascript.js
@@ -0,0 +1,26 @@
+//alert('Seja Bem-vindo');
+
+let docePreferido = "pudim";
+console.log(docePreferido);
+
+let meuNumero = 20;
+meuNumero = 1.20;
+console.log(meuNumero);
+
+let valido = true;
+console.log('valido1:', valido)
+valido = false;
+console.log('valido2:', valido);
+
+let minhaReceita = ["farinha", "bacon", "manteiga"];
+console.log(minhaReceita.length);
+console.log(minhaReceita);
+
+let chocolate = {
+    marca: "Nestlé",
+    tamanho: 90,
+    peso: "grama",
+    formato: "barra",
+    validade: 2019,
+};
+console.log(chocolate);
